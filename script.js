cb3da37cf0e3c208004d117b8c5c57c9c6f208fc
ozcanemre <ozcan.emre1608@gmail.com>
2022-05-09 14:04:52 +0200

    html css

---
+++ script.js
@@ -0,0 +1,20 @@
+let name = document.querySelector('.name')
+let description = document.querySelector('.description')
+let date = document.querySelector('.date')
+let add = document.querySelector('.add')
+let remaining=document.querySelector('.remaining')
+
+
+
+let now = new Date()
+
+
+
+
+function createtodo(){
+    console.log("hello");
+}
+
+
+
+add.addEventListener('click',createtodo)
